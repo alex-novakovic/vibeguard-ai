6ce5e125cc5d31cfef75a556961d76e3c0d5f7fa
injacmilica <milicainjac912@gmail.com>
2026-05-06 12:05:39 +0200

    Added feature log pydantic

--- data/state.py
+++ data/state.py
@@ -1,13 +1,14 @@
 from data.schemas import VisionDoc
+from data.schemas import FeatureLogItem
 
 class ProjectState:
-    def __init__(self, vision_doc: VisionDoc, feature_log: dict):
+    def __init__(self, vision_doc: VisionDoc, feature_log: FeatureLogItem):
         self.vision_doc = vision_doc
         self.feature_log = feature_log
         self.active_feature_id = None
         self.current_cycle_tokens = 0
 
-    def update_state(self, vision_doc: VisionDoc, feature_log: dict, feature_id: str, tokens: int):
+    def update_state(self, vision_doc: VisionDoc, feature_log: FeatureLogItem, feature_id: str, tokens: int):
         self.vision_doc = vision_doc
         self.feature_log = feature_log
         self.active_feature_id = feature_id
